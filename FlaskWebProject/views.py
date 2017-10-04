f71b7878e5dd87107da9ccacfe30c62677ebffdd
Humza Akhtar <humza@Humzas-MacBook-Pro.local>
2017-10-05 00:27:44 +0800

    del6

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -8,8 +8,19 @@ from FlaskWebProject import app
 
 @app.route('/')
 @app.route('/home')
-def home():
+def index():
     """Renders the home page."""
     return render_template(
         'home.html'
     )
+
+
+# about page route function
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+#dashboarding page for showing dashboard
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')
