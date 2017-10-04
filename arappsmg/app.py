14469dab0d49946aa7376a02ea5783437a3f264f
Humza Akhtar <humza@Humzas-MacBook-Pro.local>
2017-10-04 21:34:55 +0800

    test

--- arappsmg/app.py
+++ arappsmg/app.py
@@ -36,7 +36,6 @@ from flask import Flask, render_template, flash, redirect, url_for, session, log
 from data import Articles
 from sqlalchemy import exc
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-from passlib.hash import sha256_crypt
 from functools import wraps
 from werkzeug import secure_filename
 import os
