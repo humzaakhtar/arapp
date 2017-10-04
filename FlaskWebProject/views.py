a89ad86199a9d396ed091f77d4c47e0136971584
Humza Akhtar <humza@Humzas-MacBook-Pro.local>
2017-10-05 00:25:30 +0800

    del5

---
+++ FlaskWebProject/views.py
@@ -0,0 +1,15 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from FlaskWebProject import app
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'home.html'
+    )
